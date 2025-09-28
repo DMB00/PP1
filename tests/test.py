6a548ca36853c1c9bdc8c287e593571d9f8bd8b9
Egor(DMB) <qwesa372@gmail.com>
2025-09-28 22:28:15 +0300

    10.2 homework

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
 # Создайте test.py файл и запустите его
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 # Тестируем проблемные случаи
 test_cases = [
